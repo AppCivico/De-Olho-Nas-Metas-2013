aa08de04cd622d5e68e87d28a21f89c996fa718b
GianVizzotto <gfvizzotto@gmail.com>
2014-10-21 13:46:41 -0200

    Removendo blib do git

--- api/blib/lib/SMM/Model/DB.pm
+++
@@ -1,7 +0,0 @@
-
-package SMM::Model::DB;
-use strict;
-use base 'Catalyst::Model::DBIC::Schema';
-
-__PACKAGE__->config( schema_class => 'SMM::Schema', );
-
--- api/blib/lib/SMM/myapp.psgi
+++
@@ -1,8 +0,0 @@
-use strict;
-use warnings;
-use lib 'lib';
-use SMM;
-
-my $app = SMM->apply_default_middlewares(SMM->psgi_app);
-$app;
-
