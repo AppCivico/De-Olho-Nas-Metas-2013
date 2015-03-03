56a3f1794478f3e38758ecba707c6385f806c250
shinAware <shin@aware.com.br>
2015-03-03 16:28:50 -0300

    FIX Página de Projetos + galeria de imagens

--- web/root/static/js/home_site.js
+++ web/root/static/js/home_site.js
@@ -19,4 +19,23 @@ $(document).ready(function() {
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
     });	
+	
+	var iv;
+	
+	$(".image-gallery .arrow.left .glyphicon").mousedown(function(){
+		var div = $(this).parent().parent().find(".contents");
+		iv = setInterval(function(){
+            div.scrollLeft( div.scrollLeft() - 4);
+        },20);
+	});
+	$(".image-gallery .arrow.right .glyphicon").mousedown(function(){
+		var div = $(this).parent().parent().find(".contents");
+		iv = setInterval(function(){
+            div.scrollLeft( div.scrollLeft() + 4);
+        },20);
+	});
+	
+	$('.image-gallery .arrow.left .glyphicon,.image-gallery .arrow.right .glyphicon').on('mouseup mouseleave', function(){
+        clearInterval(iv);
+    });
 });
