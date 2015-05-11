9235507531156faf518306984acc0376085dbadc
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-05-11 18:18:14 -0300

    admin painel
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

--- api/script/project_number.pl
+++ api/script/project_number.pl
@@ -39,9 +39,12 @@ while ( my $row = $csv->getline($fh) ) {
     my $project =
       $schema->resultset('Project')->search( { project_number => $row->[1] } )
       ->next;
+
     use DDP;
+    p $row->[1];
     my $update_project;
-    unless ( defined $project ) {
+    if ( !$project ) {
+        p $project;
         my $url =
           URI->new("http://planejasampa.prefeitura.sp.gov.br/metas/api");
 
@@ -50,7 +53,8 @@ while ( my $row = $csv->getline($fh) ) {
         my $resp  = $furl->get( $url->as_string );
         my $value = decode_json $resp->content;
         warn "value";
-        p $value;
+        print $row->[1], "\n";
+        print $value->{name}, "\n";
         $update_project =
           $schema->resultset('Project')
           ->search( { name => { ilike => $value->{name} } } )
@@ -60,29 +64,6 @@ while ( my $row = $csv->getline($fh) ) {
           ->search( { project_number => $row->[1] } )->next
           if $update_project;
     }
-    next unless defined $project->type;
-    next if $project->type->id eq 8;
-
-    my $milestone =
-      $schema->resultset('Milestone')
-      ->search(
-        { sequence => $row->[3], project_type_id => $project->type->id } )
-      ->next;
-    push @{ $data->{ $project->id } },
-      ( ( $milestone->percentage * $row->[4] ) / 100 )
-      if $project and $milestone;
-    $schema->resultset('ProjectMilestone')->create(
-        {
-            project_id => $project->id,
-            milestone  => $row->[3],
-            status     => $row->[4],
-        }
-    );
 }
 
-#for my $d ( keys $data ) {
-#    my $sum = sum @{ $data->{$d} };
-#    $schema->resultset('Project')->find($d)->update( { percentage => $sum } );
-#    warn "soma: $d : $sum";
-#}
 $csv->eol("\r\n");
--- web/lib/WebSMM/Controller/Admin/Goal.pm
+++ web/lib/WebSMM/Controller/Admin/Goal.pm
@@ -56,6 +56,11 @@ sub add : Chained('base') : PathPart('new') : Args(0) {
 
 }
 
+sub edit : Chained('object') : PathPart('edit') : Args(0) {
+    my ( $self, $c, $id ) = @_;
+
+}
+
 =encoding utf8
 
 =head1 AUTHOR
