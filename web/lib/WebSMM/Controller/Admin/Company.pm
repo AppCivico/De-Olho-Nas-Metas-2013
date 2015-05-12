21af4cd23d27e97bf025e1eb4c15fa88c001a339
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-05-12 15:35:57 -0300

    company and subprefecture
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

---
+++ web/lib/WebSMM/Controller/Admin/Company.pm
@@ -0,0 +1,79 @@
+package WebSMM::Controller::Admin::Company;
+use Moose;
+use namespace::autoclean;
+use utf8;
+
+BEGIN { extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+WebSMM::Controller::Admin::Company - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+=head2 index
+
+=cut
+
+sub base : Chained('/admin/base') : PathPart('company') : CaptureArgs(0) {
+    my ( $self, $c, $id ) = @_;
+
+}
+
+sub object : Chained('base') : PathPart('') : CaptureArgs(1) {
+    my ( $self, $c, $id ) = @_;
+
+    my $api = $c->model('API');
+
+    $api->stash_result( $c, [ 'companies', $id ], stash => 'company_obj' );
+
+}
+
+sub detail : Chained('object') : PathPart('') Args(0) {
+    my ( $self, $c ) = @_;
+}
+
+sub index : Chained('base') : PathPart('') : Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $api = $c->model('API');
+
+    $api->stash_result( $c, 'companies' );
+}
+
+sub add : Chained('base') : PathPart('new') : Args(0) {
+    my ( $self, $c ) = @_;
+
+}
+
+sub edit : Chained('object') : PathPart('edit') : Args(0) {
+    my ( $self, $c, $id ) = @_;
+    my $api = $c->model('API');
+    $api->stash_result( $c, 'companies' );
+    $c->stash->{select_companies} =
+      [ map { [ $_->{id}, $_->{name} ] } @{ $c->stash->{companies} } ];
+
+}
+
+=encoding utf8
+
+=head1 AUTHOR
+
+development,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ web/lib/WebSMM/Controller/Admin/Form/Company.pm
@@ -0,0 +1,73 @@
+package WebSMM::Controller::Admin::Form::Company;
+use Moose;
+use namespace::autoclean;
+use DateTime;
+use JSON::XS;
+
+BEGIN { extends 'Catalyst::Controller' }
+
+sub base : Chained('/admin/form/base') : PathPart('') : CaptureArgs(0) {
+    my ( $self, $c ) = @_;
+}
+
+sub process : Chained('base') : PathPart('company') : Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $api    = $c->model('API');
+    my $params = { %{ $c->req->params } };
+    warn 1;
+    $api->stash_result(
+        $c, ['companies'],
+        method => 'POST',
+        body   => $params
+    );
+    use DDP;
+    p $c->stash->{error};
+    if ( $c->stash->{error} ) {
+        $c->detach( '/form/redirect_error', [] );
+    }
+
+    $c->detach( '/form/redirect_ok',
+        [ '/admin/company/index', {}, 'Cadastrado com sucesso!' ] );
+}
+
+sub process_edit : Chained('base') : PathPart('company') : Args(1) {
+    my ( $self, $c, $id ) = @_;
+
+    my $api    = $c->model('API');
+    my $params = { %{ $c->req->params } };
+
+    $api->stash_result(
+        $c, [ 'companies', $id ],
+        method => 'PUT',
+        body   => $params
+    );
+
+    if ( $c->stash->{error} ) {
+        $c->detach( '/form/redirect_error', [] );
+    }
+    else {
+        $c->detach( '/form/redirect_ok',
+            [ '/admin/company/index', {}, 'Alterado com sucesso!' ] );
+    }
+}
+
+sub process_delete : Chained('base') : PathPart('remove_company') : Args(1) {
+    my ( $self, $c, $id ) = @_;
+
+    my $api = $c->model('API');
+
+    $api->stash_result( $c, [ 'companies', $id ], method => 'DELETE' );
+
+    if ( $c->stash->{error} ) {
+        $c->detach( '/form/redirect_error', [] );
+    }
+    else {
+        $c->detach( '/form/redirect_ok',
+            [ '/admin/company/index', {}, 'Removido com sucesso!' ] );
+    }
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
