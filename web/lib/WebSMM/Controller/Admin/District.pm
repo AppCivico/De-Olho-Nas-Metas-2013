3cf55312f79e120509e1cfac5a4e75d7b05a927b
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-05-29 12:54:03 -0300

    alter
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

--- web/lib/WebSMM/Controller/Admin/District.pm
+++ web/lib/WebSMM/Controller/Admin/District.pm
@@ -45,9 +45,6 @@ sub index : Chained('base') : PathPart('') : Args(0) {
     my $api = $c->model('API');
 
     $api->stash_result( $c, 'regions' );
-    use DDP;
-    p $c->stash->{regions};
-    warn 1;
 }
 
 sub add : Chained('base') : PathPart('new') : Args(0) {
