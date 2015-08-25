e15b25b282a32abc229c4469413d6cf563bd414e
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-08-25 15:42:46 -0300

    Contratos
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

--- api/lib/SMM/Schema/Result/CompanyDocument.pm
+++ api/lib/SMM/Schema/Result/CompanyDocument.pm
@@ -1,4 +1,5 @@
 use utf8;
+
 package SMM::Schema::Result::CompanyDocument;
 
 # Created by DBIx::Class::Schema::Loader
@@ -32,7 +33,8 @@ extends 'DBIx::Class::Core';
 
 =cut
 
-__PACKAGE__->load_components("InflateColumn::DateTime", "TimeStamp", "PassphraseColumn");
+__PACKAGE__->load_components( "InflateColumn::DateTime", "TimeStamp",
+    "PassphraseColumn" );
 
 =head1 TABLE: C<company_documents>
 
@@ -63,17 +65,17 @@ __PACKAGE__->table("company_documents");
 =cut
 
 __PACKAGE__->add_columns(
-  "id",
-  {
-    data_type         => "integer",
-    is_auto_increment => 1,
-    is_nullable       => 0,
-    sequence          => "company_documents_id_seq",
-  },
-  "url_name",
-  { data_type => "text", is_nullable => 0 },
-  "company_id",
-  { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
+    "id",
+    {
+        data_type         => "integer",
+        is_auto_increment => 1,
+        is_nullable       => 0,
+        sequence          => "company_documents_id_seq",
+    },
+    "url_name",
+    { data_type => "text", is_nullable => 0 },
+    "company_id",
+    { data_type => "integer", is_foreign_key => 1, is_nullable => 1 },
 );
 
 =head1 PRIMARY KEY
@@ -99,21 +101,19 @@ Related object: L<SMM::Schema::Result::Company>
 =cut
 
 __PACKAGE__->belongs_to(
-  "company",
-  "SMM::Schema::Result::Company",
-  { id => "company_id" },
-  {
-    is_deferrable => 0,
-    join_type     => "LEFT",
-    on_delete     => "NO ACTION",
-    on_update     => "NO ACTION",
-  },
+    "company",
+    "SMM::Schema::Result::Company",
+    { id => "company_id" },
+    {
+        is_deferrable => 0,
+        join_type     => "LEFT",
+        on_delete     => "NO ACTION",
+        on_update     => "NO ACTION",
+    },
 );
-
 
 # Created by DBIx::Class::Schema::Loader v0.07042 @ 2015-08-24 11:55:32
 # DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:r5S5k8hXp/PbyiSbtW0hsA
-
 
 # You can replace this text with custom code or comments, and it will be preserved on regeneration
 __PACKAGE__->meta->make_immutable;
