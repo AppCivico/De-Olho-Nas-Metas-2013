866346cd339010ae3d86f6db2b06590826d1335c
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-08-25 18:27:36 -0300

    sqitch deploy
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

---
+++ deploy_db/revert/0003-create-company-document.sql
@@ -0,0 +1,7 @@
+-- Revert flipr:0003-create-company-document from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ deploy_db/verify/0003-create-company-document.sql
@@ -0,0 +1,7 @@
+-- Verify flipr:0003-create-company-document on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
