d9c0ced769b466032af533c3d667f9b0ca4ffcaa
Renan Carvalho <renan.azevedo.carvalho@gmail.com>
2015-10-13 12:11:05 -0300

    organization type
    Signed-off-by: Renan Carvalho <renan.azevedo.carvalho@gmail.com>

---
+++ deploy_db/revert/0006-create_organization_type.sql
@@ -0,0 +1,7 @@
+-- Revert flipr:0006-create_organization_type from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ deploy_db/verify/0006-create_organization_type.sql
@@ -0,0 +1,7 @@
+-- Verify flipr:0006-create_organization_type on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
