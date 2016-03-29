4fd4cce5a8f2c373cecbc63e934d50c8b785ba8b
RenatoCRON <renato.cron@gmail.com>
2016-03-29 19:20:49 -0300

    new configus

--- up_api.sh
+++ up_api.sh
@@ -7,4 +7,6 @@ sleep 2
 
 cd /home/donm/De-Olho-Nas-Metas/api
 
+sqitch deploy
+
 EMAIL_ADMIN=your@email.com starman -l 127.0.0.1:3060 --workers 2 --preload-app --error-log /home/donm/logs/api.error.log --daemonize smm.psgi
