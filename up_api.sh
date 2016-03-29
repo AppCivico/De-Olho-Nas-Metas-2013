c216f0d8c190fc295a08a91cf0b71e628bd3fdbd
RenatoCRON <renato.cron@gmail.com>
2016-03-29 19:58:02 -0300

    install deps before starting starman

--- up_api.sh
+++ up_api.sh
@@ -9,4 +9,6 @@ cd /home/donm/De-Olho-Nas-Metas/api
 
 sqitch deploy
 
+cpanm -n installdeps .
+
 EMAIL_ADMIN=your@email.com starman -l 127.0.0.1:3060 --workers 2 --preload-app --error-log /home/donm/logs/api.error.log --daemonize smm.psgi
--- up_web.sh
+++ up_web.sh
@@ -7,4 +7,6 @@ sleep 2
 
 cd /home/donm/De-Olho-Nas-Metas/web
 
+cpanm -n installdeps .
+
 starman -l 127.0.0.1:5040 --workers 2 --preload-app --error-log /home/donm/logs/web.error.log --daemonize websmm.psgi
