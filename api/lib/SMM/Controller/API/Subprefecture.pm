1f9a96ad22b0ecf141066af709428e69ba6728fb
Renato Santos <renato.cron@gmail.com>
2016-07-09 17:34:23 -0300

    Update Subprefecture.pm

--- api/lib/SMM/Controller/API/Subprefecture.pm
+++ api/lib/SMM/Controller/API/Subprefecture.pm
@@ -37,7 +37,7 @@ sub result_GET {
 
     my $subprefecture = $c->stash->{subprefecture};
     my $name          = $subprefecture->organizations->get_column('name');
-    my @projects      = $c->model('DB::Region')->search({'me.subprefecture_id' => $subprefecture->id})->projects;
+    my @projects      = $c->model('DB::Region')->search({'me.subprefecture_id' => $subprefecture->id})->next->projects;
 
     $self->status_ok(
         $c,
